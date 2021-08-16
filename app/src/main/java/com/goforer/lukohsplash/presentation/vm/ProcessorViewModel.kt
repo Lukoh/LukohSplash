892dca613cc40f6add7e57255745cde2aea819f0
Lukoh <lukoh.nam@gmail.com>
2021-08-16 12:40:03 +0900

    Code Refactoring

--- app/src/main/java/com/goforer/lukohsplash/presentation/vm/ProcessorViewModel.kt
+++ app/src/main/java/com/goforer/lukohsplash/presentation/vm/ProcessorViewModel.kt
@@ -3,10 +3,12 @@ package com.goforer.lukohsplash.presentation.vm
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.goforer.lukohsplash.domain.UseCase
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-open class ProcessorViewModel<Value>(private val useCase: UseCase<Value>, params: Params) : ViewModel() {
+open class ProcessorViewModel<Value>(private val useCase: UseCase<Value>, params: Params) :
+    ViewModel() {
     private var initValue: Value? = null
 
     private val _value = MutableStateFlow(initValue)
@@ -26,4 +28,20 @@ open class ProcessorViewModel<Value>(private val useCase: UseCase<Value>, params
 
         _value.value = initValue
     }
+
+    // You can implement code blow:
+    // Just go to Main or Challenge branch here if you'd like to know how to implement
+    /*
+    private var initValue: Value? = null
+    val value = useCase.run(viewModelScope, params).flatMapLatest {
+        flow {
+            initValue = it
+            emit(it)
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = initValue
+    )
+     */
 }
